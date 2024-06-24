ffbbc70dc78cd3d2aa5913dd8705d2a74ff300af
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2024-06-24 16:39:26 +0500

    under dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.smartcarfin"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.smartcarfin"
         minSdk = 24
--- app/src/main/java/com/example/smartcarfin/MainActivity.kt
+++ app/src/main/java/com/example/smartcarfin/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.smartcarfin
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.smartcarfin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+
+            binding.switch1.setOnCheckedChangeListener { compoundButton, checked ->
+                if(checked){
+                    binding.layout1.setBackgroundColor(Color.RED)
+                }
+                else{
+                    binding.layout1.setBackgroundColor(Color.BLACK)
+                }
+            }
         }
     }
 }
